336adbc3945c94492380e3a348c34c624deaf415
wayne.workman2012 <wayne.workman2012@gmail.com>
2016-08-28 22:38:16 -0500

    Tell SELinux about the new ssh port.

--- mailIP.sh
+++ mailIP.sh
@@ -4,8 +4,8 @@ tun0IP=$( /usr/sbin/ip addr show | /usr/bin/grep tun0 | /usr/bin/grep -o "inet [
 
 
 
-USER="ThePIAUsername"
-PASSWORD="ThePIAPassword"
+USER="TheUsername"
+PASSWORD="ThePassword"
 
 
 client_id1=$(/usr/bin/head -n 100 /dev/urandom | /usr/bin/md5sum | /usr/bin/tr -d " -")  
@@ -15,7 +15,8 @@ port1=$(echo $port1 | awk -F ':|}' '{print $2}')
 #configure sshd to listen to new forwarded port.
 /usr/bin/sed -i '/Port /d' /etc/ssh/sshd_config
 /usr/bin/echo "Port $port1" >> /etc/ssh/sshd_config
-systemctl restart sshd
+/usr/sbin/semanage port -a -t ssh_port_t -p tcp $port1
+/usr/bin/systemctl restart sshd
 
 
 #define iptables absolute location:
@@ -31,15 +32,14 @@ client_id2=$(/usr/bin/head -n 100 /dev/urandom | /usr/bin/md5sum | /usr/bin/tr -
 port2=$(/usr/bin/wget -q --post-data="user=$USER&pass=$PASSWORD&client_id=$client_id2&local_ip=$tun0IP" -O - 'https://www.privateinternetaccess.com/vpninfo/port_forward_assignment' | /usr/bin/head -1)
 port2=$(echo $port2 | awk -F ':|}' '{print $2}')
 
-#internal web server IP:
-webServer=10.0.0.2
+webServer=10.0.0.4
 
 $iptables -I PREROUTING -t nat -i tun0 -p tcp --dport $port2 -j DNAT --to $webServer:80
-$iptables -I FORWARD -p tcp -d $webServer --dport $port2 -j ACCEPT
+$iptables -I FORWARD -p tcp -d webServer --dport $port2 -j ACCEPT
+service iptables save
 
 
-
-message="Public IP\n$publicIP\n\ntun0 IP\n$tun0IP\n\nenp1s8 IP\n$enp1s8IP\n\nPort1\n$port1\n\nPort2\n$port2\n\nclient_id1=$client_id1\n\nclient_id2=$client_id2\n"
+message="Public IP=$publicIP\n\nPort1=$port1\n\nPort2=$port2\n\nAlways be a rock star,\nWayne"
 
 echo -e "$message" | /usr/bin/mail -s "IP Update" wayne.workman2012@gmail.com
 
